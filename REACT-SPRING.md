2c4a592a9fef6b29800717745d3e8bd47f56c090
Timo Stollenwerk <tisto@plone.org>
2018-09-18 19:02:03 +0200

    Add minimal react-spring example

---
+++ REACT-SPRING.md
@@ -0,0 +1,7 @@
+Install react-spring via npm:
+
+```
+npm install react-spring
+```
+
+import { Spring } from 'react-spring'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Spring } from 'react-spring'
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
+    <Spring
+      from={{ fontSize: 10, number: 0 }}
+      to={{ fontSize: 120, number: 100 }}
+    >
+      {({ fontSize, number }) => (
+        <div style={{ fontSize }}>{number.toFixed(2)}</div>
+      )}
+    </Spring>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
