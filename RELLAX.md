453328479cbd8abc437471f0249a7e6babe21d26
Timo Stollenwerk <tisto@plone.org>
2018-12-28 07:54:38 +0100

    Add basic relax-parallax example.

---
+++ RELLAX.md
@@ -0,0 +1,26 @@
+# Rellax-Parallax
+
+## Installation
+
+```yarn add react-rellax````
+
+## Add Parallax
+
+```<div
+  style={{
+    height: '100vh',
+    background: 'red',
+  }}
+>
+  <Parallax
+    style={{
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+    data-rellax-speed="-4"
+  >
+    Hello World
+  </Parallax>
+</div>```
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import Parallax from 'react-rellax'
 
-import Header from './header'
 import './layout.css'
 
 const Layout = ({ children, data }) => (
@@ -28,13 +28,30 @@ const Layout = ({ children, data }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            height: '100vh',
+            background: 'red',
+          }}
+        >
+          <Parallax
+            style={{
+              height: '100vh',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            data-rellax-speed="-4"
+          >
+            Hello World
+          </Parallax>
+        </div>
+
         <div
           style={{
             margin: '0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
           }}
         >
           {children}
