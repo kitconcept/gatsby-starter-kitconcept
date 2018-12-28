eec84ca1811f2e61551fb4f1a8ef18f7705936ca
Timo Stollenwerk <tisto@plone.org>
2018-12-28 06:58:37 +0100

    Add non-working zooming version.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import ZoomableImage from '../components/ZoomableImage'
+import myimage from '../images/gatsby-icon.png'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
+    <ZoomableImage
+      id='myid'
+      src={myimage}
+      dataOriginal={myimage}
+    />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
