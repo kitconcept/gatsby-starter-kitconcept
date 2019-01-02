cc2cce4f6e96e202586b7fe8741802d4e9dbc49a
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-01-02 13:21:47 +0100

    Add Gatsby purgeCSS plugin and related config to make it work with semanticUI

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -18,5 +20,23 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     'gatsby-plugin-less',
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        content: [
+          path.join(process.cwd(), 'src/**/!(*.d).{ts,js,jsx,tsx}'),
+          path.join(
+            process.cwd(),
+            'node_modules/semantic-ui-less/!(*.d).{ts,js,jsx,tsx}'
+          ),
+        ],
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
   ],
 }
